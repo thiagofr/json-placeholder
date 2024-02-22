bf760decd94f568b8a984394b33ad48e01f90076
Thiago França <thiagofr4nca@gmail.com>
2024-02-22 19:55:07 -0300

    Configuração das bibliotecas inicialmente necessárias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,10 +37,19 @@ android {
 
 dependencies {
 
+    val nav_version = "2.7.7"
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.squareup.retrofit2:adapter-guava:2.9.0")
+    implementation("com.squareup.retrofit2:adapter-guava:2.9.0")
+    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("io.insert-koin:koin-android:3.5.3")
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
